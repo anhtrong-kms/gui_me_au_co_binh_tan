cc96817b49ff6c1f81800d927ae674ca661e8768
anhtrong-kms <anhtrongnguyen52@gmail.com>
2023-12-04 08:11:11 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Hello mẹ Âu Cơ",
+    introTitle: 'Nguyễn Phương Hà Âu Cơ',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ thiếu KPI quài 🥴
+    Lạc vào nơi ấy lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Bấm đây nè^^',
+    title: 'Mẹ LA Âu Cơ ơi 🥰',
+    desc: 'Tháng này mẹ cho con đủ 100% KPI mà không cần làm nhiều nhaaaaaaaaa',
+    btnYes: 'Okk con ❤️',
+    btnNo: 'Không nha 😗',
+    question: 'Sao mẹ đồng ý dạ',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tại mẹ yêu con nên muốn con đủ 100% KPI mà an nhàn ớ',
+    mess: 'Con biết mà 🥰. Yêu mẹ nhiều nhiều 😘😘',
+    messDesc: 'Tối thứ 4 gặp quẫy nho',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/jafirst.anhtrong.1607/'
+}
